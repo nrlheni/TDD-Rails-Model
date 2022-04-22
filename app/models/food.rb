b78bc12f03b8e95e0fe5e89f8268c1652596c2f3
nrlheni <nurulakhni12@gmail.com>
2022-04-22 00:49:04 +0000

    Add validation for Model, invalid with non numeric value and price less than 0.01

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,16 @@
 class Food < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  
+  def self.by_letter(letter)
+    where("name LIKE ?", "#{letter}%").order(:name)
+  end
+
+  # def self.less_than_two_words
+  #   name = :name.split
+  #   if name.size < 2
+  #     errors.add(:name, "must be greater than or equal to two words")
+  #   end
+  # end
+
 end
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -41,4 +41,62 @@ RSpec.describe Food, type: :model do
     
     expect(food2.errors[:name]).to include("has already been taken")
   end
+  it 'is invalid price with non numeric value' do
+    food = Food.new(
+      name: "Ayam Bakar",
+      description: "Ayamnya dibakar",
+      price: "sepuluh ribu"
+    )
+    food.valid?
+
+    expect(food.errors[:price]).to include("is not a number")
+  end
+  it 'is invalid price less than 0.01' do
+    food = Food.new(
+      name: "Ayam Bakar",
+      description: "Ayamnya dibakar",
+      price: 0.001
+    )
+    food.valid?
+
+    expect(food.errors[:price]).to include("must be greater than or equal to 0.01")
+  end
+
+  describe 'self#by_letter' do
+    it "should return a sorted array of results that match" do
+      food1 = Food.create(
+        name: "Nasi Uduk",
+        description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+        price: 10000.0
+      )
+
+      food2 = Food.create(
+        name: "Kerak Telor",
+        description: "Betawi traditional spicy omelette made from glutinous rice cooked with egg and served with serundeng.",
+        price: 8000.0
+      )
+
+      food3 = Food.create(
+        name: "Nasi Semur Jengkol",
+        description: "Based on dongfruit, this menu promises a unique and delicious taste with a small hint of bitterness.",
+        price: 8000.0
+      )
+
+      expect(Food.by_letter("N")).to eq([food3, food1])
+    end
+  end
+
+  # describe 'self#less_than_two_words' do
+  #   it 'is invalid name less than two words' do
+  #     food = Food.new(
+  #       name: "Ayam",
+  #       description: "Ayamnya digoreng",
+  #       price: 5000.0
+  #     )
+  #     food.valid?
+      
+  #     expect(food.less_than_two_words)
+  #   end
+  # end
+
 end
